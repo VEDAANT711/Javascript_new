27b838046cf925de559b36c52c48bd0a9d31675e
VEDANT711 <gaikwadvedant2@gmail.com>
2024-02-19 13:17:17 +0530

    added

---
+++ script4.js
@@ -0,0 +1,45 @@
+console.log("hello")
+var balance = 5000
+console.log(balance)
+balance = 6000
+console.log(balance)
+
+const time = 24
+console.log(time)
+
+
+
+
+function add(i,j){
+    console.log(i+j)
+    console.log(i*j)
+    console.log(i/j)
+    console.log(i-j)
+    console.log(i%j)
+}
+add(7,8)
+
+
+
+function add1(i,j){
+    //return i + j
+    //return i - j
+    //return i == j
+    //return i != j
+    //return i % j
+    //return i ^ j
+    //return i / j
+    //return i * j
+}
+var a1 = add1(43,56)
+console.log(a1)
+
+
+
+
+
+
+
+
+
+
