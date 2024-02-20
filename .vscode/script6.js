a3ccd373339f6a3a970bc9f949acc1d27d683fa8
VEDANT711 <gaikwadvedant2@gmail.com>
2024-02-20 15:48:28 +0530

    added

---
+++ .vscode/script6.js
@@ -0,0 +1,73 @@
+var seher = "Nagpur"
+switch(seher){
+    case  "Pune":
+         console.log("MAHARASHTRA")
+         break
+    case  "Jaipur":
+         console.log("RAJASTHAN")
+         break
+    case  "Banglore":
+        console.log("KARNATAKA")
+        break
+    case  "Kolkata":
+        console.log("WESTBENGAL")   
+        break
+    default :
+         console.log("SEHER NOT FOUND IN THE LIST")         
+
+}
+
+
+
+
+ let x1 = 10
+ let x2 = 5
+ let x3 = 2
+ let isGreatest = true
+ switch(isGreatest){
+
+     case x1 > x2 && x1 > x3:
+         console.log("x1 is greater")
+         break
+     case x2 > x1 && x2 > x3:
+         console.log("x2 is greater")
+         break 
+     default:
+         console.log("x3 is greater")
+
+ }
+
+
+for (let i = 1; i <= 3; i++) {
+    console.log(i)  
+}
+
+
+
+for(let i = 50 ; i >= 5 ; i = i - 5){ 
+    console.log(i) 
+}    
+
+
+for (let i = 6 ; i <= 60 ; i = i + 6){
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
