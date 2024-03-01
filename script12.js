efd42aa6c58c0b7ddbd166499d3b431bf1100634
VEDANT711 <gaikwadvedant2@gmail.com>
2024-03-01 20:52:50 +0530

    added

---
+++ script12.js
@@ -0,0 +1,75 @@
+let first_name = "vedu"
+console.log(first_name)
+
+let last_name = "gaikwad"
+console.log(last_name)
+
+let middle_name = "shashikant"
+console.log(middle_name)
+
+
+let fn = "Vedant"
+let ln = "Gaikwad"
+
+
+console.log("My firstname is "+fn+" and my lastname is "+ln)
+
+console.log(`My FirstName is ${fn} and my lastName is ${ln}`)
+
+
+
+let a = 10
+let b = 15
+let c ="hello"
+
+console.log(a+b+c)
+
+console.log(c+a+b)
+
+console.log(b+c+a)
+
+
+
+
+
+let city = "pune"
+console.log(city)
+let e = city [0]
+console.log(e)
+
+console.log(city[0])
+console.log(city[1])
+console.log(city[2])
+console.log(city[3])
+
+
+
+
+let city3 = "chandrapur"
+console.log(city3.length)
+for(let i=0 ; i < 10 ; i++){
+    console.log(city3[i])
+}
+
+for (let i = city3.length - 1; i >= 0 ; i--){
+    console.log(city3[i])
+}
+
+
+let city4 = "nagpur"
+let i1 =  0
+
+while(i1 < city4.length){
+    console.log(city4[i1])
+    i1 = i1 + 1
+}
+
+
+let i2 = city4.length - 1
+while(i2 >= 0){
+    console.log(city4[i2])
+        i2 = i2  - 1
+}
+
+
+
