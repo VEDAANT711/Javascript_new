c2fb95f88c1541a3f9867c27f395a19ade0dd706
VEDANT711 <gaikwadvedant2@gmail.com>
2024-03-04 20:52:14 +0530

    added

---
+++ script13.js
@@ -0,0 +1,76 @@
+let first_name = "Vedant"
+console.log(first_name)
+console.log(typeof(first_name))
+
+let q1 = first_name.length
+console.log(q1)
+
+last_name = "Gaikwad"
+
+
+let s1 = last_name.toUpperCase()
+console.log(s1)
+
+
+let s2 = last_name.toLowerCase()
+console.log(s2)
+
+
+let s3 = last_name.toLocaleUpperCase()
+console.log(s3)
+
+
+let s4 = last_name.toUpperCase().toLowerCase()
+console.log(s4)
+
+
+let fruit = "apple"
+let sj = fruit.startsWith("app")
+let s5 = fruit.startsWith("a")
+let s6 = fruit.startsWith("A")
+let s7 = fruit.endsWith("e")
+let s8 = fruit.endsWith("le")
+let s9 = fruit.endsWith("lle")
+
+console.log(sj,s5,s6,s7,s8,s9)
+
+
+let vegetable = "cauliflower"
+let s10 = vegetable.includes('i')
+let s11 = vegetable.includes('fl')
+let s12 = vegetable.includes('Caul')
+let s13 = vegetable.indexOf('o')
+let s14 = vegetable.indexOf('l')
+
+console.log(s10,s11,s12,s13,s14)
+
+
+let a11 = ' hello'
+console.log(a11)
+
+console.log(a11.length)
+s15 = a11.trimStart()
+console.log(s15.length)
+
+
+a11 = ' hello '
+console.log(a11.length)
+s16 = a11.trimEnd()
+console.log(s16.length)
+
+
+a11 = ' hello '
+s17  = a11.trim()
+console.log(s17.length)
+
+
+info = 'i am learning javascript'
+s18 = info.replace("javascript","python")
+console.log(s18)
+
+
+
+
+
+
+
