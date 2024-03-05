b57d35968f2d7570ee7f0f899e141b1a0a19de36
VEDANT711 <gaikwadvedant2@gmail.com>
2024-03-05 20:00:39 +0530

    added

---
+++ script15.js
@@ -0,0 +1,63 @@
+let first_name = "chandrapur"
+console.log(first_name)
+
+let q1 = first_name.slice(2)
+let q2 = first_name.slice(2,8)
+console.log(q1,q2)
+console.log(first_name.slice(-8))
+console.log(first_name.slice(1,-1))
+console.log(first_name.slice(-7,8))
+console.log(first_name.slice(-1,-3))
+
+
+
+
+let last_name  = "gaikwad"
+let fullName = last_name.concat("").concat("vedant")
+console.log(fullName)
+
+
+let full_name = "vedant gaikwad 7066558537"
+let info = full_name.split(" ")
+console.log(info)
+console.log(full_name.split("a"))
+
+
+
+let city2 = "nagpur"
+let e2 = city2.indexOf('g')
+console.log(e2)
+
+
+let info3 = "i am learning js"
+let e4 = info3.replace("js","python")
+console.log(e4)
+
+
+let e5 =  info3.repeat(4) 
+console.log(e5)
+
+let city10 = "Wardha"
+console.log(city10.charAt(2))
+
+console.log(city10.charCodeAt(3))
+
+
+let info4 = "iamcoder"
+console.log(info4.substring(1,5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
