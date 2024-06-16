d15f37712cc6c4014815269cba6248067e6ea5b4
VEDANT711 <gaikwadvedant2@gmail.com>
2024-06-16 23:57:50 +0530

    added

---
+++ dom_pr_2/script.js
@@ -0,0 +1,11 @@
+let para = document.querySelector('p')
+console.log(para)
+
+let byId = document.querySelector('onee')
+console.log(byId)
+
+let byClassName = document.querySelector(".two")
+console.log(byClassName)
+
+let byAtt = document.querySelector('p'[n = "name"])
+console.log(byAtt)
